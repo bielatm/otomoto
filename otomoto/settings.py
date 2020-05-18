b49450d351a3cfead35aa774958189869d085472
bielatm <bielat.marlena@gmail.com>
2020-05-18 19:43:55 +0200

    configure postgres database

--- otomoto/settings.py
+++ otomoto/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'otomoto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'otomoto',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '5432',
+        'HOST': 'localhost',
     }
 }
 
