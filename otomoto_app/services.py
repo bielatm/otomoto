d9c772fd2bcc9895f2ba9f88117e76c08d82b486
bielatm <bielat.marlena@gmail.com>
2020-05-22 19:46:14 +0200

    new service ParsePage

--- otomoto_app/services.py
+++ otomoto_app/services.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 class GetOtoMotoPage():
@@ -6,3 +7,10 @@ class GetOtoMotoPage():
     def execute(self, url):
         response = requests.get(url)
         return response if response.ok else None
+
+
+class ParsePage():
+
+    def execute(self, html):
+        parsed_html = BeautifulSoup(html)
+        return parsed_html.body.find('div', attrs={'class': 'offers list'})
